4b14c8200cc2a5c9438584d57297eb63489ae5b1
HALIM_LEE <rimha93@gmail.com>
2020-04-29 10:59:50 +0900

    .

--- untitled1/venv/Lab 0428.py
+++ untitled1/venv/Lab 0428.py
@@ -7,39 +7,26 @@ from sklearn import linear_model
 
 df = pd.read_csv('/Users/halim/Downloads/bmi_data_lab3.csv')
 
+print(df)  #Print dataset statistical data, feature names & data types
 
 
-# extremelyweak_wt = []
-# weak_wt = []
-# normal_wt = []
-# overweight_wt = []
-# obesity_wt = []
+# bit_mask0 = df['BMI'] == 0
+# df_eweak = df[bit_mask0]
 #
-# extremelyweak_ht = []
-# weak_ht = []
-# normal_ht = []
-# overweight_ht = []
-# obesity_ht = []
-
-bit_mask0 = df['BMI'] == 0
-df_eweak = df[bit_mask0]
-
-bit_mask1 = df['BMI'] == 1
-df_weak = df[bit_mask1]
-
-bit_mask2 = df['BMI'] == 2
-df_normal = df[bit_mask2]
-
-bit_mask3 = df['BMI'] == 3
-df_overweight = df[bit_mask3]
-
-bit_mask4 = df['BMI'] == 4
-df_obesity = df[bit_mask4]
-
-
-print(df_normal['Height (Inches)'])
-
-
+# bit_mask1 = df['BMI'] == 1
+# df_weak = df[bit_mask1]
+#
+# bit_mask2 = df['BMI'] == 2
+# df_normal = df[bit_mask2]
+#
+# bit_mask3 = df['BMI'] == 3
+# df_overweight = df[bit_mask3]
+#
+# bit_mask4 = df['BMI'] == 4
+# df_obesity = df[bit_mask4]
+#
+#
+#
 # plt.hist(df_eweak['Height (Inches)'], bins=10)
 # plt.title('extremely weak')
 # plt.xlabel('Height (Inches)')
@@ -47,25 +34,25 @@ print(df_normal['Height (Inches)'])
 # plt.show()
 #
 # plt.hist(df_weak['Height (Inches)'], bins=10)
-# plt.title('extremely weak')
+# plt.title('weak')
 # plt.xlabel('Height (Inches)')
 # plt.ylabel('Number of student')
 # plt.show()
 #
 # plt.hist(df_normal['Height (Inches)'], bins=10)
-# plt.title('extremely weak')
+# plt.title('normal')
 # plt.xlabel('Height (Inches)')
 # plt.ylabel('Number of student')
 # plt.show()
 #
 # plt.hist(df_overweight['Height (Inches)'], bins=10)
-# plt.title('extremely weak')
+# plt.title('overweight')
 # plt.xlabel('Height (Inches)')
 # plt.ylabel('Number of student')
 # plt.show()
 #
 # plt.hist(df_obesity['Height (Inches)'], bins=10)
-# plt.title('extremely weak')
+# plt.title('obesity')
 # plt.xlabel('Height (Inches)')
 # plt.ylabel('Number of student')
 # plt.show()
@@ -106,33 +93,49 @@ print(row_nan.sum(axis=0))
 print("\nExtract all rows without NAN")
 print(nan_df.dropna(axis=0))
 
-# print('<fillna with mean function>\n')
-# print(nan_df.fillna(df.mean()))
-# print('\n')
-#
-#
-# print('<fillna with median function>\n')
-# print(nan_df.fillna(df.median()))
-# print('\n')
-#
-#
-# print('<fillna with ffill function>\n')
-# print(nan_df.fillna(method='ffill'))
-# print('\n')
-#
-#
-# print('<fillna with bfill function>\n')
-# print(nan_df.fillna(method='bfill'))
-# print('\n')
-#
 
-nan_df.head()
-height = nan_df['Height (Inches)'].values
-weight = nan_df['Weight (Pounds)'].values
+print('<fillna with mean function>\n')
+print(nan_df.fillna(df.mean()))
+print('\n')
+
+
+print('<fillna with median function>\n')
+print(nan_df.fillna(df.median()))
+print('\n')
+
+
+print('<fillna with ffill function>\n')
+print(nan_df.fillna(method='ffill'))
+print('\n')
+
+
+print('<fillna with bfill function>\n')
+print(nan_df.fillna(method='bfill'))
+print('\n')
+
+
+clean_nan_df = nan_df.dropna(axis=0)
+clean_nan_df.head()
+c_height = clean_nan_df['Height (Inches)'].values
+c_weight = clean_nan_df['Weight (Pounds)'].values
+
 reg = linear_model.LinearRegression()
-reg.fit(height[:, np.newaxis], weight)
+reg.fit(c_height[:, np.newaxis], c_weight)
+
+px = np.array([c_height.min()-1, c_height.max()+1])
+py = reg.predict(px[:, np.newaxis])
+plt.scatter(c_height, c_weight)
+plt.plot(px, py, color='black')
+plt.show()
 
 
-
-
-
+# nan_df.head()
+#
+# print('--------------------------------')
+# print(nan_df[(~nan_df.notnull()).any(axis=1)])
+#
+# df2 = nan_df[(~nan_df.notnull()).any(axis=1)]
+#
+# for i in range (18):
+#     if
+#
